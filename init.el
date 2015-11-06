985f8ee9c4367906ea84a152dbb842143d046a39
Oscar Qi <oscar.Qi@Honeywell.com>
2015-11-06 10:22:48 +0800

    v4

--- init.el
+++ init.el
@@ -1,22 +1,20 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
-;;	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;        '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
 
-;; (setq url-proxy-services '(("no_proxy" . "work\\.com")
-;;                            ("http" . "127.0.0.1:1080")))
-
-(org-babel-load-file "~/.emacs.d/emacs.org")
-
+(org-babel-load-file
+ (expand-file-name "emacs.org"
+		   user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ ;; If there is more than one, they won't work right.              
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
