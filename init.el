3699a003d000283db4062b1f6ebce6e23957bac9
Oscar Qi <oscar.Qi@Honeywell.com>
2015-11-12 10:55:36 +0800

    1.update x command for eshell 2.avoid the secure theme prompt 3.only hook the smartscan-mode to c and elisp mode

--- init.el
+++ init.el
@@ -7,9 +7,6 @@
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize) ;; You might already have this line
 
-(org-babel-load-file
- (expand-file-name "emacs.org"
-		   user-emacs-directory))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,3 +23,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(org-babel-load-file
+ (expand-file-name "emacs.org"
+		   user-emacs-directory))
