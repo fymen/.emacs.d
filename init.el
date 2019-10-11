e472e2c083ab97b6f763a6e014ee8f0a3c525c22
fengmao qi <fengmao.qi@gmail.com>
2019-10-11 10:50:00 +0800

    Disable golden-raion and open file as root

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (docker ox-hugo leetcode circe skewer-mode js2-mode web-mode sdcv elfeed-goodies elfeed esh-buf-stack helm-projectile blog-admin yasnippet whole-line-or-region wgrep-ag wc-mode use-package smartscan smartparens smart-mode-line rainbow-mode rainbow-delimiters paradox page-break-lines org-bullets org olivetti nyan-mode multiple-cursors magit keyfreq ibuffer-vc hungry-delete highlight-symbol hideshow-org helm-pass helm-gtags helm-fuzzier helm-flx hc-zenburn-theme golden-ratio god-mode gnuplot expand-region dtrt-indent dired-filetype-face company-flx company-c-headers comment-dwim-2 color-identifiers-mode cd-compile beacon avy anzu)))
+    (docker-compose-mode dockerfile-mode docker ox-hugo leetcode circe skewer-mode js2-mode web-mode sdcv elfeed-goodies elfeed esh-buf-stack helm-projectile blog-admin yasnippet whole-line-or-region wgrep-ag wc-mode use-package smartscan smartparens smart-mode-line rainbow-mode rainbow-delimiters paradox page-break-lines org-bullets org olivetti nyan-mode multiple-cursors magit keyfreq ibuffer-vc hungry-delete highlight-symbol hideshow-org helm-pass helm-gtags helm-fuzzier helm-flx hc-zenburn-theme golden-ratio god-mode gnuplot expand-region dtrt-indent dired-filetype-face company-flx company-c-headers comment-dwim-2 color-identifiers-mode cd-compile beacon avy anzu)))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
