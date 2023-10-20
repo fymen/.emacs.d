cdb45d6fbde0712bfae53c248f40dcd628074cb7
Oscar Qi <fengmao.qi@gmail.com>
2023-10-19 20:57:02 -0400

    To be compatible with private.org

--- init.el
+++ init.el
@@ -4,9 +4,9 @@
 
 (org-babel-load-file
  (expand-file-name "emacs.org"
- 		   user-emacs-directory))
+  		           user-emacs-directory))
 
-(if (file-exists-p "private.org")
-    (org-babel-load-file
-     (expand-file-name "private.org"
-		       user-emacs-directory)))
+(if (file-exists-p
+     (setq private-file
+           (expand-file-name "private.org" user-emacs-directory)))
+    (org-babel-load-file private-file))
