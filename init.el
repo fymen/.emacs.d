74cd8063b405a5209bc741802baa23d42759fd92
Oscar Qi <fengmao.qi@gmail.com>
2023-12-20 09:31:52 -0500

    Make Elfeed work properly

--- init.el
+++ init.el
@@ -8,5 +8,5 @@
 
 (if (file-exists-p
      (setq private-file
-           (expand-file-name "private.org" user-emacs-directory)))
+           (expand-file-name "private.org" "~/personal/")))
     (org-babel-load-file private-file))
